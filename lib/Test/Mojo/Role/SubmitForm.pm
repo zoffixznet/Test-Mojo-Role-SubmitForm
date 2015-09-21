6f2441e1f50b9c58212296867e63f9fb7f8d22a6
Zoffix Znet <cpan@zoffix.com>
2015-09-21 17:34:44 -0400

    First full commit

---
+++ lib/Test/Mojo/Role/SubmitForm.pm
@@ -0,0 +1,192 @@
+package Test::Mojo::Role::SubmitForm;
+
+use Mojo::Base -base;
+use Encode;
+use Carp qw/croak/;
+use Role::Tiny;
+
+# VERSION
+
+sub __val {
+  my $dom = shift;
+
+  # "option"
+  my $tag = $dom->tag;
+  return $dom->{value} // $dom->text if $tag eq 'option';
+
+  # "select"
+  return $dom->find('option[selected]')->map(sub{__val($_)})->to_array
+    if $tag eq 'select';
+
+  # "textarea", "input" or "button"
+  return $tag eq 'textarea' ? $dom->text : $dom->{value};
+}
+
+sub click_ok {
+    my ( $self, $selector, $extra_params ) = @_;
+    $extra_params ||= {};
+
+    my $el = $self->tx->res->dom->at($selector)
+        or croak "Did not find element matching selector $selector";
+    unless ( $el->tag eq 'form' ) {
+        if ( $el->attr('type') eq 'image' ) {
+            $extra_params->{ $el->attr('name') . '.x' } = 1;
+            $extra_params->{ $el->attr('name') . '.y' } = 1;
+        }
+        else {
+            $extra_params->{ $el->attr('name') } = $el->attr('value');
+        }
+
+        $el = $el->ancestors('form')->first;
+    }
+
+    for ( sort keys %$extra_params ) {
+        next unless ref $extra_params->{$_} eq 'CODE';
+        $extra_params->{$_} = $extra_params->{$_}->(
+            __val( $el->at("[name=$_]") )
+        );
+    }
+
+    my $tx = $self->ua->build_tx(
+        $el->attr('method')||'GET' => $el->attr('action')
+            => form => {
+                $self->_get_controls($el),
+                %$extra_params,
+            },
+    );
+
+    $self->request_ok( $tx );
+
+    $self;
+}
+
+sub _get_controls {
+    my ( $self, $el ) = @_;
+
+    map +( $_->attr('name') => __val($_) ),
+        $el->find(
+            'input:not([type=button]):not([type=submit]):not([type=image])'
+            . ':not([type=image]):not([type=checkbox]):not([type=radio]),'
+            . '[type=checkbox][checked], [type=radio][checked],'
+            . 'select, textarea'
+        )->each;
+}
+
+q|
+The fantastic element that explains the appeal of games to
+many developers is neither the fire-breathing monsters nor the
+milky-skinned, semi-clad sirens; it is the experience of
+carrying out a task from start to finish without any change
+in the user requirements.
+|;
+
+__END__
+
+=encoding utf8
+
+=for stopwords Znet Zoffix app  Subrefs subrefs
+
+=head1 NAME
+
+Test::Mojo::Role::SubmitForm - Test::Mojo role that allows to submit forms
+
+=head1 SYNOPSIS
+
+=for pod_spiffy start code section
+
+    use Test::More;
+    use Test::Mojo::WithRoles 'SubmitForm';
+    my $t = Test::Mojo::WithRoles->new('MyApp');
+
+    # Submit a form without clicking any buttons: pass selector to the form
+    $t->get_ok('/')->status_is(200)->click_ok('form#one')->status_is(200);
+
+    # Click a particular submit
+    $t->get_ok('/')->status_is(200)->click_ok('[type=submit]')->status_is(200);
+
+    # Submit a form while overriding form data
+    $t->get_ok('/')->status_is(200)
+        ->click_ok('form#one', {
+            input1        => '42',
+            select1       => [ 1..3 ],
+            other_select  => sub { my $r = shift; [ @$r, 42 ] },
+            another_input => sub { shift . 'offix'}
+        })->status_is(200);
+
+    done_testing;
+
+=for pod_spiffy end code section
+
+=head1 DESCRIPTION
+
+A L<Test::Mojo> role that allows you submit forms, optionally overriding
+any of the values already present
+
+=head1 METHODS
+
+You have all the methods provided by L<Test::Mojo>, plus these:
+
+=head2 C<click_ok>
+
+    $t->click_ok('form');
+    $t->click_ok('#button');
+
+    $t->click_ok('#button', {
+        input1        => '42',
+        select1       => [ 1..3 ],
+        other_select  => sub { my $r = shift; [ @$r, 42 ] },
+        another_input => sub { shift . 'offix'}
+    })
+
+First parameter specifies a CSS selector matching a C<< <form> >> you want to
+submit or a particular C<< <button> >>, C<< <input type="submit"> >>,
+or C<< <input type="image"> >> you want to click.
+
+Specifying a second parameter allows you to override the form control values:
+the keys are C<name="">s of controls to override and values can be either
+plain scalars (use arrayrefs for multiple values) or subrefs. Subrefs
+will be evaluated and their first C<@_> element will be the current value
+of the form control.
+
+=head1 SEE ALSO
+
+L<Test::Mojo>, L<Mojo::DOM>
+
+=for pod_spiffy hr
+
+=head1 REPOSITORY
+
+=for pod_spiffy start github section
+
+Fork this module on GitHub:
+L<https://github.com/zoffixznet/Test-Mojo-Role-SubmitForm>
+
+=for pod_spiffy end github section
+
+=head1 BUGS
+
+=for pod_spiffy start bugs section
+
+To report bugs or request features, please use
+L<https://github.com/zoffixznet/Test-Mojo-Role-SubmitForm/issues>
+
+If you can't access GitHub, you can email your request
+to C<bug-test-mojo-role-SubmitForm at rt.cpan.org>
+
+=for pod_spiffy end bugs section
+
+=head1 AUTHOR
+
+=for pod_spiffy start author section
+
+=for pod_spiffy author ZOFFIX
+
+=for pod_spiffy end author section
+
+=head1 LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
+See the C<LICENSE> file included in this distribution for complete
+details.
+
+=cut
---
+++ t/Test/MyApp.pm
@@ -0,0 +1,51 @@
+
+use Mojolicious::Lite;
+get '/' => 'index';
+any '/test' => sub {
+    my $c = shift;
+
+    $c->render( json => $c->req->params->to_hash );
+};
+
+app->start;
+
+1;
+
+__DATA__
+
+@@index.html.ep
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+
+<form action="/test" method="POST" id="one">
+  <p>Test</p>
+  <input type="text" name="a" value="A">
+  <input type="checkbox" checked name="b" value="B">
+  <input type="checkbox" name="c" value="C">
+  <input type="radio" name="d" value="D">
+  <input type="radio" checked name="e" value="E">
+  <input type="radio" name="e" value="Z">
+  <select name="f">
+    <option value="F">G</option>
+    <optgroup>
+      <option>H</option>
+      <option selected>I</option>
+    </optgroup>
+    <option value="J" selected>K</option>
+  </select>
+  <select name="n"><option>N</option></select>
+  <select name="l"><option selected>L</option></select>
+  <textarea name="m">M</textarea>
+  <button name="o" value="O">No!</button>
+  <input type="button" name="s" value="S">
+  <input type="submit" name="p" value="P">
+  <input type="image" src="" name="z" value="Z">
+</form>
+<form action="/test" id="two"><input type="submit" name="q" value="Q"></form>
+<form action="/test" id="three"><input type="button" name="r" value="R"></form>
+<form action="/test" id="four">
+    <input type="image" src="" name="z" value="Z">
+</form>
+<form action="/test" id="five"></form>
